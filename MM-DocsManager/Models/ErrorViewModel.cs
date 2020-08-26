b21a883e0aab23b481f5e69f7f1d3d2bcdd1fa07
gubiarpa <billy.arredondo@pucp.pe>
2020-08-25 20:36:39 -0500

    Web Project Created
    Se eligió un proyecto MVC con Net Core 3.1

---
+++ MM-DocsManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MM_DocsManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
