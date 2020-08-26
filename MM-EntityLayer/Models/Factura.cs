7104ab61c4d72a60dff7293b86c30eb1ba41c045
gubiarpa <billy.arredondo@pucp.pe>
2020-08-25 22:44:55 -0500

    Factura Model Created

---
+++ MM-EntityLayer/Models/Factura.cs
@@ -0,0 +1,19 @@
+﻿using MM_EntityLayer.Models.Behavior;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MM_EntityLayer.Models
+{
+    public class Factura : IGuid
+    {
+        public Guid Id { get; set; }
+        public int NumeroSerie { get; set; }
+        public int NumeroDocumento { get; set; }
+        public string NombreCliente { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Monto { get; set; }
+        public int Cantidad { get; set; }
+        public string NombreProducto { get; set; }
+    }
+}
