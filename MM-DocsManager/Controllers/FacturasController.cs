1ff35f48b497d79a667cf6bfbec9a0ada3f42803
gubiarpa <billy.arredondo@pucp.pe>
2020-08-26 02:34:59 -0500

    Factura View Visualization
    Se puede apreciar la visualización de facturas.

---
+++ MM-DocsManager/Controllers/FacturasController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MM_DataLayer;
+using MM_DocsManager.Controllers.Behavior;
+
+namespace MM_DocsManager.Controllers
+{
+    public class FacturasController : MainControllerBase
+    {
+        public FacturasController(DBContextManager context) : base(context)
+        {
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var facturas = await _context.Facturas.ToListAsync();
+
+            return View(facturas);
+        }
+    }
+}
